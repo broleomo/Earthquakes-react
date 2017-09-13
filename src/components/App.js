7b63ee7af53e8503158e7d67408d1e68f72c85ec
broleomo <brooklynnmoor@gmail.com>
2017-09-13 15:04:26 -0400

    first try

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import '../styles/App.css';
+import Earthquakes from './Earthquakes';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App-header">
+      <div className="title">
+      <div className = "my-header">
+      </div>
+      </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/EarthquakeInfo.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import earthquakes from '../data/earthquakes'
+
+export default class EarthquakeInfo extends Component {
+    render(){
+      return (
+        <div className="earthquake-title">
+
+        </div>
+      )
+    }
+}
